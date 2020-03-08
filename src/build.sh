78b1a322a8738828eaaa09184566f067e31d86bf
abstractxan <priyansh0506@gmail.com>
2020-03-08 16:49:41 +0530

    Updated ./main

--- src/build.sh
+++ src/build.sh
@@ -5,5 +5,3 @@ rm ../site/*
 g++ -DDEBUG -Wall -Wpedantic -Wshadow -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -g -Og -fsanitize=address -fsanitize=undefined main.cpp -o main
 
 ./main
-
-rm ./main
